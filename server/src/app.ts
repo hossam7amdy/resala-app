bd968234c97cd8f02cb8122c2b3aaad4a8ed3a40
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-03-03 16:21:52 +0200

    Update build and test scripts

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ import api from './router';
 const app = express();
 
 const corsConfig: CorsOptions = {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: '*',
 };
 
 app.use(cors(corsConfig));
