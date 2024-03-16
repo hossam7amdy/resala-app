9df341ebfaab7d8b5ffa45ba6f0bc946dcda1d9f
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-16 06:46:22 +0200

    Remove unused imports in testserver.ts

--- server/src/__test__/testserver.ts
+++ server/src/__test__/testserver.ts
@@ -1,5 +1,3 @@
-import { config } from 'dotenv';
-import path from 'path';
 import superset from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
@@ -9,8 +7,6 @@ let client: TestAgent<superset.Test>;
 
 export async function getTestServer() {
   if (!client) {
-    config({ path: path.join(__dirname, '..', '..', '/.env.local') });
-
     const app = createExpressApp(false);
     client = superset(app);
   }
