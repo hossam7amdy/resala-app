b55c7656e1541a2a7b31fd7df8dfc40e4e4b001e
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-18 08:41:55 +0200

    Refactor inventoryService module.

--- server/src/controller/color-controller/color-validator.ts
+++ server/src/controller/color-controller/color-validator.ts
@@ -3,15 +3,13 @@ import { RequestHandler } from 'express';
 
 import { BadRequestError } from '../../utils/api-errors';
 
-const CreateColorSchema = ColorSchema.omit({ id: true });
-
 export const validateCreateColor: RequestHandler = (req, _, next) => {
   const { arName, enName, code } = req.body;
   if (!arName || !enName || !code) {
     return next(new BadRequestError('name, and code are required'));
   }
 
-  const colorValidation = CreateColorSchema.safeParse({ arName, enName, code });
+  const colorValidation = ColorSchema.safeParse({ arName, enName, code });
   if (!colorValidation.success) {
     return next(new BadRequestError(colorValidation.error.issues[0].message));
   }
--- server/src/controller/stock-controller/stock-validator.ts
+++ server/src/controller/stock-controller/stock-validator.ts
@@ -3,15 +3,13 @@ import { RequestHandler } from 'express';
 
 import { BadRequestError } from '../../utils/api-errors';
 
-const CreateStockSchema = StockSchema.omit({ id: true });
-
 export const validateCreateStock: RequestHandler = (req, _, next) => {
   const { productId, colorId, sizeId, quantity } = req.body;
   if (!productId || !colorId || !sizeId || !quantity) {
     return next(new BadRequestError('productId, colorId, sizeId, and quantity are required'));
   }
 
-  const stockValidation = CreateStockSchema.safeParse(req.body);
+  const stockValidation = StockSchema.safeParse(req.body);
   if (!stockValidation.success) {
     return next(new BadRequestError(stockValidation.error.issues[0].message));
   }
