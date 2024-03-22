978833ad696fdb25fe97047e629e1665f91f6c52
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-22 21:41:20 +0200

    Refactor ROUTE tests

--- server/src/__test__/ROUTE/user.test.ts
+++ server/src/__test__/ROUTE/user.test.ts
@@ -3,7 +3,7 @@ import { ENDPOINT_CONFIGS } from '@resala/shared';
 import superset from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
-import { userService } from '../service';
+import { userService } from '../../service';
 import { getTestServer } from './testserver';
 
 describe('TEST /users endpoint', () => {
