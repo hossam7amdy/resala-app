6e4bd7e8196222f83b6b43efadbd85e4c52bfbd6
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-26 06:42:58 +0200

    update fetchCall to fetch in paymob files

--- server/src/lib/paymob/checkout.ts
+++ server/src/lib/paymob/checkout.ts
@@ -1,5 +1,5 @@
 import { ENV } from '../../config';
-import { fetchCall } from '../../utils/fetch';
+import { fetch } from '../../utils/fetch';
 
 const CURRENCY = 'EGP';
 const PAYMOB_API_URL = ENV.PAYMOB_API_URL;
@@ -45,7 +45,7 @@ interface CheckoutResponse {
  */
 export async function checkout(payload: CheckoutRequest): Promise<CheckoutResponse> {
   try {
-    const response = await fetchCall.post(`${PAYMOB_API_URL}/acceptance/payment_keys`, {
+    const response = await fetch.post(`${PAYMOB_API_URL}/acceptance/payment_keys`, {
       ...payload,
       currency: CURRENCY,
       integration_id: PAYMOB_INTEGRATION_ID,
