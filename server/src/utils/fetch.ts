a39ea508d576498dac3c6e950b8017c5aad22592
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-26 06:42:05 +0200

    Refactor fetchCall to use httpCall

--- server/src/utils/fetch.ts
+++ server/src/utils/fetch.ts
@@ -1,49 +1,8 @@
-async function call<Request, Response>({
-  url,
-  method,
-  body,
-  headers,
-}: {
-  url: string;
-  method: string;
-  body?: Request;
-  headers?: Record<string, string>;
-}): Promise<Response> {
-  try {
-    const response = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-        ...headers,
-      },
-      body: JSON.stringify(body),
-    });
+import { httpCall } from './http-call';
 
-    const contentType = response.headers.get('content-type');
-    const isJson = contentType && contentType.includes('application/json');
-
-    if (!response.ok) {
-      let errorMessage = `Request failed with status ${response.status}`;
-      if (isJson) {
-        const responseData = (await response.json()) as any;
-        errorMessage = responseData.error || errorMessage;
-      } else {
-        const responseData = await response.text();
-        errorMessage = responseData || errorMessage;
-      }
-      throw new Error(errorMessage);
-    }
-
-    return isJson ? ((await response.json()) as Response) : ((await response.text()) as Response);
-  } catch (error) {
-    console.error('Failed to call API', error);
-    throw new Error('Failed to call API');
-  }
-}
-
-export const fetchCall = {
+export const fetch = {
   get<Response>(url: string, headers?: Record<string, string>): Promise<Response> {
-    return call({ url, method: 'GET', headers });
+    return httpCall({ url, method: 'GET', headers });
   },
 
   post<Request, Response>(
@@ -51,7 +10,7 @@ export const fetchCall = {
     body: Request,
     headers?: Record<string, string>
   ): Promise<Response> {
-    return call({ url, method: 'POST', body, headers });
+    return httpCall({ url, method: 'POST', body, headers });
   },
 
   put<Request, Response>(
@@ -59,10 +18,10 @@ export const fetchCall = {
     body: Request,
     headers?: Record<string, string>
   ): Promise<Response> {
-    return call({ url, method: 'PUT', body, headers });
+    return httpCall({ url, method: 'PUT', body, headers });
   },
 
   delete<Response>(url: string, headers?: Record<string, string>): Promise<Response> {
-    return call({ url, method: 'DELETE', headers });
+    return httpCall({ url, method: 'DELETE', headers });
   },
 };
