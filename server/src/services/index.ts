a2b86e1c85d89df054347e308837f6946828b37e
Hossam Hamdy <hossam7amdy@gmail.com>
2024-03-29 13:23:31 +0200

    Update folder name

---
+++ server/src/services/index.ts
@@ -0,0 +1,7 @@
+export * as authService from './auth-service';
+export * as userService from './user-service';
+export * as inventoryService from './inventory-service';
+export * as communicationService from './communication-service';
+export * as shoppingService from './shopping-service';
+export * as orderService from './order-service';
+export * as paymentService from './payment-service';
