792b84d76ce19924829807eec748310b5eb3793a
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-04-07 17:08:52 +0200

    Refactor inventory-service

--- server/src/service/shopping-service.ts
+++ server/src/service/shopping-service.ts
@@ -1,87 +1,100 @@
+import { Color, Product, Size } from '@prisma/client';
+
+import { inventoryService } from '.';
 import prisma from '../lib/prisma';
 import { NotFoundError } from '../utils/api-errors';
 
-export async function getUserCart(userId: number) {
-  const cart = await prisma.cart.findMany({
-    select: {
-      stockId: true,
-      quantity: true,
-      stock: {
-        select: {
-          product: true,
-          color: true,
-          size: true,
-        },
-      },
-    },
-    where: { userId },
-    orderBy: { createdAt: 'desc' },
-  });
-
-  return cart.map(item => ({
-    ...item,
-    product: {
-      id: item.stock.product.id,
-      arName: item.stock.product.arName,
-      enName: item.stock.product.enName,
-      price: item.stock.product.price,
-    },
-    color: {
-      id: item.stock.color.id,
-      arName: item.stock.color.arName,
-      enName: item.stock.color.enName,
-      code: item.stock.color.code,
-    },
-    size: {
-      id: item.stock.size.id,
-      name: item.stock.size.name,
-    },
-  }));
+export interface CartItem {
+  userId: number;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+  stock: {
+    id: number;
+    quantity: number;
+    product: Product;
+    color: Color;
+    size: Size;
+  };
 }
 
-export async function addToCart(
-  userId: number,
-  cartItem: {
-    stockId: number;
-    quantity: number;
+export async function getUserCart(userId: number): Promise<CartItem[]> {
+  try {
+    return await prisma.cart.findMany({
+      select: {
+        userId: true,
+        quantity: true,
+        createdAt: true,
+        updatedAt: true,
+        stock: {
+          select: {
+            id: true,
+            quantity: true,
+            product: true,
+            color: true,
+            size: true,
+          },
+        },
+      },
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new NotFoundError('User not found');
   }
-) {
-  const stock = await findStockById(cartItem.stockId);
+}
+
+export async function addItemToCart(cartItem: {
+  userId: number;
+  stockId: number;
+  quantity: number;
+}): Promise<CartItem[]> {
+  const stock = await inventoryService.findStockById(cartItem.stockId);
 
   if (stock.quantity < cartItem.quantity) {
     throw new NotFoundError('Not enough stock');
   }
 
-  await prisma.cart.upsert({
-    create: { userId, ...cartItem },
-    update: { userId, ...cartItem },
-    where: { userId_stockId: { userId, stockId: cartItem.stockId } },
-  });
+  try {
+    const cart = await prisma.cart.upsert({
+      create: cartItem,
+      update: cartItem,
+      where: {
+        userId_stockId: {
+          userId: cartItem.userId,
+          stockId: cartItem.stockId,
+        },
+      },
+    });
 
-  return getUserCart(userId);
+    return await getUserCart(cart.userId);
+  } catch (error) {
+    console.log(error);
+    throw new NotFoundError('User not found');
+  }
 }
 
-export async function removeFromCart(userId: number, stockId: number) {
+export async function removeItemFromCart(userId: number, stockId: number): Promise<CartItem[]> {
   try {
-    await prisma.cart.delete({
+    const cart = await prisma.cart.delete({
       where: { userId_stockId: { userId, stockId } },
     });
 
-    return getUserCart(userId);
+    return await getUserCart(cart.userId);
   } catch (error) {
+    console.log(error);
     throw new NotFoundError('Item not found in cart');
   }
 }
 
-export async function removeUserCart(userId: number) {
-  await prisma.cart.deleteMany({ where: { userId } });
+export async function clearUserCart(userId: number) {
+  return await prisma.cart.deleteMany({ where: { userId } });
 }
 
-export async function getUserWishlist(userId: number) {
+export async function getUserWishlist(userId: number): Promise<Product[]> {
   const wishlist = await prisma.wishlist.findMany({
-    select: {
-      product: true,
-    },
+    select: { product: true },
     where: { userId },
     orderBy: { createdAt: 'desc' },
   });
@@ -89,54 +102,33 @@ export async function getUserWishlist(userId: number) {
   return wishlist.map(item => item.product);
 }
 
-export async function addProductToWishlist(userId: number, productId: number) {
-  await findProductById(productId);
+export async function addProductToWishlist(userId: number, productId: number): Promise<Product[]> {
+  await inventoryService.findProductById(productId);
 
-  await prisma.wishlist.upsert({
+  const wishlist = await prisma.wishlist.upsert({
     create: { userId, productId },
     update: { userId, productId },
     where: { userId_productId: { userId, productId } },
   });
 
-  return getUserWishlist(userId);
+  return await getUserWishlist(wishlist.userId);
 }
 
-export async function removeProductFromWishlist(userId: number, productId: number) {
+export async function removeProductFromWishlist(
+  userId: number,
+  productId: number
+): Promise<Product[]> {
   try {
     await prisma.wishlist.delete({
       where: { userId_productId: { userId, productId } },
     });
 
-    return getUserWishlist(userId);
+    return await getUserWishlist(userId);
   } catch (error) {
     throw new NotFoundError('Product not found in wishlist');
   }
 }
 
 export async function removeUserWishlist(userId: number) {
-  await prisma.wishlist.deleteMany({ where: { userId } });
-}
-
-async function findStockById(id: number) {
-  const stock = await prisma.stock.findUnique({
-    where: { id },
-  });
-
-  if (!stock) {
-    throw new NotFoundError('Stock not found');
-  }
-
-  return stock;
-}
-
-async function findProductById(id: number) {
-  const product = await prisma.product.findUnique({
-    where: { id },
-  });
-
-  if (!product) {
-    throw new NotFoundError('Product not found');
-  }
-
-  return product;
+  return await prisma.wishlist.deleteMany({ where: { userId } });
 }
