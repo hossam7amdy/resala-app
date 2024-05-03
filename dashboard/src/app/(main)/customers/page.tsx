226c0b38fe403ac1d7c34f7cc65ec7b484e8d2e0
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-03 18:35:47 +0300

    Refactor routes

--- dashboard/src/app/(main)/customers/page.tsx
+++ dashboard/src/app/(main)/customers/page.tsx
@@ -1,6 +1,6 @@
-import { CustomerTable } from '@/components/customers/customer-table';
-import { Table } from 'antd';
-import Title from 'antd/es/typography/Title';
+import CustomersTable from '@/components/customers/table';
+import { Search } from '@/components/ui/search';
+import { Breadcrumb, Button, Col, Flex, Row, Table } from 'antd';
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
 
@@ -8,14 +8,34 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const CustomerPage = async () => {
+const CustomerPage = async ({
+  searchParams,
+}: {
+  searchParams?: { page?: string; limit?: string; query?: string };
+}) => {
+  const page = Number(searchParams?.page) || 1;
+  const limit = Number(searchParams?.limit) || 10;
+  const query = searchParams?.query || '';
+
   return (
-    <div style={{ padding: 10 }}>
-      <Title>Customers</Title>
-      <Suspense fallback={<Table loading />}>
-        <CustomerTable />
-      </Suspense>
-    </div>
+    <Row gutter={[10, 20]} style={{ padding: 20 }}>
+      <Col span={24}>
+        <Breadcrumb items={[{ title: 'Customers' }]} />
+      </Col>
+
+      <Col span={24}>
+        <Flex gap={10}>
+          <Search placeholder="Search customers" />
+          <Button type="primary">Add Customer</Button>
+        </Flex>
+      </Col>
+
+      <Col span={24}>
+        <Suspense fallback={<Table loading />}>
+          <CustomersTable page={page} limit={limit} query={query} />
+        </Suspense>
+      </Col>
+    </Row>
   );
 };
 
