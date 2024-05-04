527e0bb231297cd15a9af7644398e6ce7ab80e60
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-04 09:35:21 +0300

    Refactor categories table component and related files

--- dashboard/src/components/categories/create-form.tsx
+++ dashboard/src/components/categories/create-form.tsx
@@ -8,6 +8,8 @@ import FormItem from 'antd/es/form/FormItem';
 import Text from 'antd/es/typography/Text';
 import { useRouter } from 'next/navigation';
 
+import ErrorMessage from '../ui/error-message';
+
 const CreateForm = ({ categories }: { categories: GetCategoriesListResponse['data'] }) => {
   const router = useRouter();
   const [form] = Form.useForm();
@@ -58,7 +60,7 @@ const CreateForm = ({ categories }: { categories: GetCategoriesListResponse['dat
         </FormItem>
       </Flex>
 
-      {error?.message && <Text type="danger">{error.message}</Text>}
+      {error?.message && <ErrorMessage message={error.message} />}
 
       <Flex gap={10}>
         <FormItem noStyle>
