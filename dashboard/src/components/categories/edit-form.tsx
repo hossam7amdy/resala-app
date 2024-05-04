9b3013452cd14ae6e8719ec76fbdecc2ac07cf64
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-04 09:35:21 +0300

    Refactor categories table component and related files

--- dashboard/src/components/categories/edit-form.tsx
+++ dashboard/src/components/categories/edit-form.tsx
@@ -12,6 +12,8 @@ import FormItem from 'antd/es/form/FormItem';
 import Text from 'antd/es/typography/Text';
 import { useRouter } from 'next/navigation';
 
+import ErrorMessage from '../ui/error-message';
+
 const EditForm = ({
   category,
   categories,
@@ -77,7 +79,7 @@ const EditForm = ({
         </FormItem>
       </Flex>
 
-      {error?.message && <Text type="danger">{error.message}</Text>}
+      {error?.message && <ErrorMessage message={error.message} />}
 
       <Flex gap={10}>
         <FormItem noStyle>
