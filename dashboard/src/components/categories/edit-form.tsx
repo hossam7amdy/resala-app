53d589f6821078f6b913d0021c5c88273d6a6c5f
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-04 09:52:43 +0300

    refactor: Remove unused imports in category form components

--- dashboard/src/components/categories/edit-form.tsx
+++ dashboard/src/components/categories/edit-form.tsx
@@ -9,7 +9,6 @@ import {
 } from '@resala/shared';
 import { Button, Flex, Form, Input, Select } from 'antd';
 import FormItem from 'antd/es/form/FormItem';
-import Text from 'antd/es/typography/Text';
 import { useRouter } from 'next/navigation';
 
 import ErrorMessage from '../ui/error-message';
