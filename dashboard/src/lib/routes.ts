66b9b68dbebc89bc2a01f62a38c9a20faeaafcc0
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-05 14:56:27 +0300

    chore: Add `/colors` routes

--- dashboard/src/lib/routes.ts
+++ dashboard/src/lib/routes.ts
@@ -23,6 +23,14 @@ export const ROUTES = {
   CREATE_STOCK: (id?: string | number) => `/stocks/create?productId=${id}`,
   EDIT_STOCK: (stockId: string | number) => `/stocks/${stockId}/edit`,
 
+  COLORS: '/colors',
+  CREATE_COLOR: '/colors/create',
+  EDIT_COLOR: (id: string | number) => `/colors/${id}/edit`,
+
+  SIZES: '/sizes',
+  CREATE_SIZE: '/sizes/create',
+  EDIT_SIZE: (id: string | number) => `/sizes/${id}/edit`,
+
   ORDERS: '/orders',
   CUSTOMERS: '/customers',
 };
