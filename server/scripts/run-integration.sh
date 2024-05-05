44b43aff4094a6146725fe3a9cd7f53af5976df8
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-05 14:59:40 +0300

    refactor: Update database URL in test environment

--- server/scripts/run-integration.sh
+++ server/scripts/run-integration.sh
@@ -5,7 +5,7 @@ source $DIR/setenv.sh
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
-npx prisma migrate dev --name init
+npx prisma migrate dev
 if [ "$#" -eq  "0" ]
   then
     vitest -c ./vitest.config.integration.ts
