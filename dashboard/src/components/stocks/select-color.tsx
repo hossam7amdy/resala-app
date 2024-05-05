9a6f22e79522648a33883cbd0c82efab073ff1f7
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-05-05 14:58:30 +0300

    refactor: Add colors and sizes routes in select component

--- dashboard/src/components/stocks/select-color.tsx
+++ dashboard/src/components/stocks/select-color.tsx
@@ -1,27 +1,43 @@
 import StockColor from '@/components/ui/stock-color';
 import { listAllColors } from '@/data/colors';
-import { Select } from 'antd';
+import ROUTES from '@/lib/routes';
+import { Button, Flex, Select } from 'antd';
 import FormItem from 'antd/es/form/FormItem';
+import Link from 'next/link';
 
 const SelectColor = async () => {
   const colors = await listAllColors();
 
   return (
-    <FormItem required name="colorId" label="Color" rules={[{ required: true }]} hasFeedback>
-      <Select
-        allowClear
-        showSearch
-        placeholder="Select color"
-        options={colors.map(c => ({
-          value: c.id,
-          label: (
-            <span>
-              <StockColor color={c.code} /> {c.enName} | {c.arName}
-            </span>
-          ),
-        }))}
-      />
-    </FormItem>
+    <Flex gap={10}>
+      <FormItem
+        required
+        name="colorId"
+        label="Color"
+        rules={[{ required: true }]}
+        hasFeedback
+        style={{ flex: 1 }}
+      >
+        <Select
+          allowClear
+          showSearch
+          placeholder="Select color"
+          options={colors.map(c => ({
+            value: c.id,
+            label: (
+              <span>
+                <StockColor color={c.code} /> {c.enName} | {c.arName}
+              </span>
+            ),
+          }))}
+        />
+      </FormItem>
+      <FormItem label=" ">
+        <Link href={ROUTES.CREATE_COLOR}>
+          <Button>Add Color</Button>
+        </Link>
+      </FormItem>
+    </Flex>
   );
 };
 
