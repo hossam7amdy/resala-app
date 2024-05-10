3a596c7536d345e01bf5862231dba1f36a0098fa
hos7amdy <hossam7amdy@gmail.com>
2024-05-10 10:53:57 +0300

    refactor: Fix issues on integration test

--- server/scripts/run-integration.sh
+++ server/scripts/run-integration.sh
@@ -5,11 +5,11 @@ source $DIR/setenv.sh
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
-npx prisma migrate dev
+yarn prisma migrate dev
 if [ "$#" -eq  "0" ]
   then
     vitest -c ./vitest.config.integration.ts
 else
     vitest -c ./vitest.config.integration.ts --ui
 fi
-docker-compose down
+docker-compose down -v
