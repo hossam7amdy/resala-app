a6e3d4890b92c22fcbc85758b8baf8ac6fa2708e
hos7amdy <hossam7amdy@gmail.com>
2024-05-18 23:38:23 +0300

    refactor: Update Dockerfile, docker-compose.yml, and folder structure for server code changes

--- packages/server/scripts/run-integration.sh
+++ packages/server/scripts/run-integration.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$(cd "$(dirname "$0")" && pwd)"
-source $DIR/setenv.sh
+DOTENV_CONFIG_PATH=.env.test
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
--- packages/server/scripts/setenv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Export env vars
-export $(grep -v '^#' .env.test | xargs)
