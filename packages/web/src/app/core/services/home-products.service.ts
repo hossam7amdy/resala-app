42994b1cb1a9cb618270fa857a4f7ce40ba55667
hos7amdy <hossam7amdy@gmail.com>
2024-05-25 11:00:34 +0300

    refactor: Update API endpoints in auth, categories, and home-products services

--- packages/web/src/app/core/services/home-products.service.ts
+++ packages/web/src/app/core/services/home-products.service.ts
@@ -1,29 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { type DefaultRequestQuery, ENDPOINT_CONFIGS, Endpoints, withParams } from '@resala/shared';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeProductsService {
+  private readonly baseURL = environment.apiUrl;
   constructor(private _HttpClient: HttpClient) {}
 
-  baseURL: string = `http://ec2-13-60-47-151.eu-north-1.compute.amazonaws.com/`;
-  //base url =
-
   // Products
-
-  getProducts(): Observable<any> {
-    return this._HttpClient.get(this.baseURL + 'api/v1/products?page=1&limit=10&query=');
+  getProducts(
+    query: DefaultRequestQuery['query'] = {
+      page: 1,
+      limit: 10,
+      query: '',
+    }
+  ): Observable<any> {
+    const { url } = ENDPOINT_CONFIGS[Endpoints.getProductsList];
+    return this._HttpClient.get(
+      this.baseURL + `${url}?page=${query.page}&limit=${query.limit}&query=${query.query}`
+    );
   }
 
   //Product Details
-
   getProductDetails(id: string | null): Observable<any> {
-    return this._HttpClient.get(this.baseURL + `api/v1/products/${id}`);
+    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.getProduct], id + '');
+    return this._HttpClient.get(this.baseURL + url);
   }
 
   getProductStock(id: string | null): Observable<any> {
-    return this._HttpClient.get(this.baseURL + `api/v1/products/${id}/stocks`);
+    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.getProductStocks], id + '');
+    return this._HttpClient.get(this.baseURL + url);
   }
 }
