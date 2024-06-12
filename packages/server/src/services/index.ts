7c322bc956f81dad0cf04902e7644391618330ab
hos7amdy <hossam7amdy@gmail.com>
2024-06-12 20:03:24 +0300

    refactor: Update communication-service to use async/await for sendEmail calls

--- packages/server/src/services/index.ts
+++ packages/server/src/services/index.ts
@@ -4,7 +4,7 @@ import ReviewService from './review-service/ReviewService.js';
 export * as authService from './auth-service/auth-service.js';
 export * as userService from './user-service/user-service.js';
 export * as inventoryService from './inventory-service/index.js';
-export * as communicationService from './communication-service/communication-service.js';
+export * as communicationService from './communication-service/communicationService.js';
 export * as shoppingService from './shopping-service/shopping-service.js';
 export * as orderService from './order-service/order-service.js';
 export * as paymentService from './payment-service/payment-service.js';
