52d3e5b93c088bcf1ded0bf39e59be8042daf6c3
hos7amdy <hossam7amdy@gmail.com>
2024-06-15 16:04:58 +0300

    add response time to loggerMiddleware

--- packages/server/src/middlewares/loggerMiddleware.ts
+++ packages/server/src/middlewares/loggerMiddleware.ts
@@ -3,15 +3,24 @@ import type { RequestHandler } from 'express';
 import { logger } from '../lib/logger/index.js';
 
 /** logging incoming requests */
-export const loggerMiddleware: RequestHandler = (req, _, next) => {
-  logger.info(
-    JSON.stringify({
-      method: req.method,
-      path: req.path,
-      body: Object.keys(req.body).length ? req.body : undefined,
-      query: Object.keys(req.query).length ? req.query : undefined,
-    })
-  );
+export const loggerMiddleware: RequestHandler = (req, res, next) => {
+  const startHrTime = process.hrtime();
+
+  res.on('finish', () => {
+    const elapsedHrTime = process.hrtime(startHrTime);
+    const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+
+    logger.info(
+      JSON.stringify({
+        method: req.method,
+        path: req.path,
+        statusCode: res.statusCode,
+        responseTime: `${elapsedTimeInMs.toFixed(3)}ms`,
+        body: Object.keys(req.body).length ? req.body : undefined,
+        query: Object.keys(req.query).length ? req.query : undefined,
+      })
+    );
+  });
 
   return next();
 };
