68e092ab308989710953fe6d9b8f4b9b69e42c34
hos7amdy <hossam7amdy@gmail.com>
2024-06-23 09:04:30 +0300

    refactor: Include user attributes in order retrieval

--- packages/server/src/repositories/OrderRepository.ts
+++ packages/server/src/repositories/OrderRepository.ts
@@ -1,24 +1,23 @@
 import type { Prisma, PrismaClient } from '@prisma/client';
 import type { Address, DefaultFilters, Order, OrderItem } from '@resala/shared';
 
-const USER_ATTRIBUTES = {
-  select: {
-    id: true,
-    email: true,
-    isVerified: true,
-    phone: true,
-    firstName: true,
-    lastName: true,
-    role: true,
-    lastLogin: true,
-    createdAt: true,
-    updatedAt: true,
-  },
-};
-
 const ORDER_ATTRIBUTES = {
   orderItems: true,
   paymentDetails: true,
+  user: {
+    select: {
+      id: true,
+      email: true,
+      isVerified: true,
+      phone: true,
+      firstName: true,
+      lastName: true,
+      role: true,
+      lastLogin: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  },
   shippingDetails: {
     select: {
       id: true,
@@ -32,7 +31,7 @@ const ORDER_ATTRIBUTES = {
 
 interface CreateOrderInput {
   userId: number;
-  address: Omit<Address, 'id' | 'createdAt' | 'updatedAt'>;
+  address: Omit<Address, 'id'>;
   paymentMethod: Order['paymentMethod'];
   items: Pick<OrderItem, 'name' | 'price' | 'quantity' | 'color' | 'size'>[];
   shipping: number;
@@ -103,7 +102,7 @@ export default class OrderRepository {
     const [count, orders] = await this.prisma.$transaction([
       this.prisma.order.count({ where: filters }),
       this.prisma.order.findMany({
-        include: { user: USER_ATTRIBUTES, ...ORDER_ATTRIBUTES },
+        include: ORDER_ATTRIBUTES,
         where: filters,
         take: limit,
         skip: (page - 1) * limit,
