7bf633afd50d82038d7762457696b5c419992da1
hos7amdy <hossam7amdy@gmail.com>
2024-06-29 23:23:45 +0300

    refactor: Update table column titles for stocks and products

--- packages/server/src/models/seed.js
+++ packages/server/src/models/seed.js
@@ -58,110 +58,110 @@ const main = async () => {
     ],
   });
 
-  // create products
-  const categories = await prisma.category.findMany({
-    orderBy: { createdAt: 'asc' },
-  });
-  await prisma.product.createMany({
-    data: [
-      {
-        categoryId: categories[0].id,
-        arName: 'فستان سهرة',
-        enName: 'Evening Dress',
-        arDescription: 'فستان سهرة طويل',
-        enDescription: 'Long Evening Dress',
-        price: 100,
-        imageKey: 'evening-dress.jpg',
-        imageUrl: `${process.env.APP_URL}/uploads/evening-dress.jpg`,
-      },
-      {
-        categoryId: categories[0].id,
-        arName: 'فستان زفاف',
-        enName: 'Wedding Dress',
-        arDescription: 'فستان زفاف طويل',
-        enDescription: 'Long Wedding Dress',
-        price: 200,
-        imageKey: 'wedding-dress.jpg',
-        imageUrl: `${process.env.APP_URL}/uploads/wedding-dress.jpg`,
-      },
-      {
-        categoryId: categories[1].id,
-        arName: 'بلوزة قطن',
-        enName: 'Cotton Blouse',
-        arDescription: 'بلوزة كتان',
-        enDescription: 'Cotton Blouse',
-        price: 400,
-        imageKey: 'cotton-blouse.jpg',
-        imageUrl: `${process.env.APP_URL}/uploads/cotton-blouse.jpg`,
-      },
-    ],
-  });
+  // // create products
+  // const categories = await prisma.category.findMany({
+  //   orderBy: { createdAt: 'asc' },
+  // });
+  // await prisma.product.createMany({
+  //   data: [
+  //     {
+  //       categoryId: categories[0].id,
+  //       arName: 'فستان سهرة',
+  //       enName: 'Evening Dress',
+  //       arDescription: 'فستان سهرة طويل',
+  //       enDescription: 'Long Evening Dress',
+  //       price: 100,
+  //       imageKey: 'evening-dress.jpg',
+  //       imageUrl: `${process.env.APP_URL}/uploads/evening-dress.jpg`,
+  //     },
+  //     {
+  //       categoryId: categories[0].id,
+  //       arName: 'فستان زفاف',
+  //       enName: 'Wedding Dress',
+  //       arDescription: 'فستان زفاف طويل',
+  //       enDescription: 'Long Wedding Dress',
+  //       price: 200,
+  //       imageKey: 'wedding-dress.jpg',
+  //       imageUrl: `${process.env.APP_URL}/uploads/wedding-dress.jpg`,
+  //     },
+  //     {
+  //       categoryId: categories[1].id,
+  //       arName: 'بلوزة قطن',
+  //       enName: 'Cotton Blouse',
+  //       arDescription: 'بلوزة كتان',
+  //       enDescription: 'Cotton Blouse',
+  //       price: 400,
+  //       imageKey: 'cotton-blouse.jpg',
+  //       imageUrl: `${process.env.APP_URL}/uploads/cotton-blouse.jpg`,
+  //     },
+  //   ],
+  // });
 
-  // create colors and sizes
-  await prisma.color.createMany({
-    data: [
-      { enName: 'white', arName: 'ابيض', code: '#ffffff' },
-      { enName: 'black', arName: 'اسود', code: '#000000' },
-      { enName: 'orange', arName: 'برتقالى', code: '#ffa500' },
-      { enName: 'pink', arName: 'وردى', code: '#ffc0cb' },
-      { enName: 'brown', arName: 'بنى', code: '#a52a2a' },
-    ],
-  });
-  await prisma.size.createMany({
-    data: [{ name: 'S' }, { name: 'M' }, { name: 'L' }, { name: 'XL' }, { name: 'XXL' }],
-  });
+  // // create colors and sizes
+  // await prisma.color.createMany({
+  //   data: [
+  //     { enName: 'white', arName: 'ابيض', code: '#ffffff' },
+  //     { enName: 'black', arName: 'اسود', code: '#000000' },
+  //     { enName: 'orange', arName: 'برتقالى', code: '#ffa500' },
+  //     { enName: 'pink', arName: 'وردى', code: '#ffc0cb' },
+  //     { enName: 'brown', arName: 'بنى', code: '#a52a2a' },
+  //   ],
+  // });
+  // await prisma.size.createMany({
+  //   data: [{ name: 'S' }, { name: 'M' }, { name: 'L' }, { name: 'XL' }, { name: 'XXL' }],
+  // });
 
-  // create product variants (stocks) 2 for each product
-  const products = await prisma.product.findMany({
-    orderBy: { createdAt: 'asc' },
-  });
-  const colors = await prisma.color.findMany({
-    orderBy: { createdAt: 'asc' },
-  });
-  const sizes = await prisma.size.findMany({
-    orderBy: { createdAt: 'asc' },
-  });
+  // // create product variants (stocks) 2 for each product
+  // const products = await prisma.product.findMany({
+  //   orderBy: { createdAt: 'asc' },
+  // });
+  // const colors = await prisma.color.findMany({
+  //   orderBy: { createdAt: 'asc' },
+  // });
+  // const sizes = await prisma.size.findMany({
+  //   orderBy: { createdAt: 'asc' },
+  // });
 
-  await prisma.stock.createMany({
-    data: [
-      {
-        productId: products[0].id,
-        colorId: colors[0].id,
-        sizeId: sizes[0].id,
-        quantity: 10,
-      },
-      {
-        productId: products[0].id,
-        colorId: colors[1].id,
-        sizeId: sizes[1].id,
-        quantity: 20,
-      },
-      {
-        productId: products[1].id,
-        colorId: colors[2].id,
-        sizeId: sizes[2].id,
-        quantity: 30,
-      },
-      {
-        productId: products[1].id,
-        colorId: colors[3].id,
-        sizeId: sizes[3].id,
-        quantity: 40,
-      },
-      {
-        productId: products[2].id,
-        colorId: colors[4].id,
-        sizeId: sizes[4].id,
-        quantity: 50,
-      },
-      {
-        productId: products[2].id,
-        colorId: colors[0].id,
-        sizeId: sizes[0].id,
-        quantity: 60,
-      },
-    ],
-  });
+  // await prisma.stock.createMany({
+  //   data: [
+  //     {
+  //       productId: products[0].id,
+  //       colorId: colors[0].id,
+  //       sizeId: sizes[0].id,
+  //       quantity: 10,
+  //     },
+  //     {
+  //       productId: products[0].id,
+  //       colorId: colors[1].id,
+  //       sizeId: sizes[1].id,
+  //       quantity: 20,
+  //     },
+  //     {
+  //       productId: products[1].id,
+  //       colorId: colors[2].id,
+  //       sizeId: sizes[2].id,
+  //       quantity: 30,
+  //     },
+  //     {
+  //       productId: products[1].id,
+  //       colorId: colors[3].id,
+  //       sizeId: sizes[3].id,
+  //       quantity: 40,
+  //     },
+  //     {
+  //       productId: products[2].id,
+  //       colorId: colors[4].id,
+  //       sizeId: sizes[4].id,
+  //       quantity: 50,
+  //     },
+  //     {
+  //       productId: products[2].id,
+  //       colorId: colors[0].id,
+  //       sizeId: sizes[0].id,
+  //       quantity: 60,
+  //     },
+  //   ],
+  // });
 
   console.log('seeded successfully 🌱');
 };
