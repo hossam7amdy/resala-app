1ac30858fda5fc6df3fbd5f20c55d08f690dd6d4
hos7amdy <hossam7amdy@gmail.com>
2024-06-29 23:25:49 +0300

    refactor: Update loggerMiddleware to remove request body from log message

--- packages/server/src/middlewares/loggerMiddleware.ts
+++ packages/server/src/middlewares/loggerMiddleware.ts
@@ -1,15 +1,10 @@
-import type { Request } from 'express';
 import morgan from 'morgan';
 
 import { logger } from '../lib/index.js';
 
-morgan.token('body', (req: Request) => {
-  return JSON.stringify(req.body);
-});
-
 /** logging incoming requests */
 export const loggerMiddleware = morgan(
-  ':method :url :status :response-time ms - :res[content-length] - :body',
+  ':method :url :status :response-time ms - :res[content-length]',
   {
     stream: { write: message => logger.info(message.trim()) },
   }
