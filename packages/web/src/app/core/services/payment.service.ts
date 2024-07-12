2420457358b96a870963ba905d22187d72baf329
hos7amdy <hossam7amdy@gmail.com>
2024-07-12 19:07:44 +0300

    refactor: Update PaymentService to use endpoint configurations

--- packages/web/src/app/core/services/payment.service.ts
+++ packages/web/src/app/core/services/payment.service.ts
@@ -1,45 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ENDPOINT_CONFIGS, withParams } from '@resala/shared';
 import { Observable } from 'rxjs';
-import { type DefaultRequestQuery, ENDPOINT_CONFIGS, Endpoints, withParams } from '@resala/shared';
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentService {
-
-  constructor(
-    private _HttpClient: HttpClient
-  ) { }
+  constructor(private _HttpClient: HttpClient) {}
   baseURL: string = `https://resala-app-6ba5cpyy5q-ey.a.run.app`;
-  myToken: any = { 'Authorization': `Bearer ${localStorage.getItem('etoken')}` }
+  myToken: any = { Authorization: `Bearer ${localStorage.getItem('etoken')}` };
 
   registerUserAddress(userAddress: any): Observable<any> {
-    const { url }: any = withParams(ENDPOINT_CONFIGS[Endpoints.createAddress]).url
-    return this._HttpClient.post(this.baseURL + '/api/v1/users/self/addresses', userAddress, {
-      headers: this.myToken
-    })
+    const { url } = ENDPOINT_CONFIGS.createAddress;
+    return this._HttpClient.post(this.baseURL + url, userAddress, {
+      headers: this.myToken,
+    });
   }
 
   getUserAddress(): Observable<any> {
-    const { url }: any = withParams(ENDPOINT_CONFIGS[Endpoints.getAddressList])
-    return this._HttpClient.get(this.baseURL + url,
-      {
-        headers: this.myToken
-      })
+    const { url } = withParams(ENDPOINT_CONFIGS.listAddress);
+    return this._HttpClient.get(this.baseURL + url, {
+      headers: this.myToken,
+    });
   }
 
   userOrder(userAddressId: string, payInfo: string, note: string): Observable<any> {
-    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.createOrder])
-    return this._HttpClient.post(this.baseURL + url, {
-      addressId: userAddressId,
-      paymentMethod: payInfo,
-      note: note
-    },
+    const { url } = ENDPOINT_CONFIGS.createOrder;
+    return this._HttpClient.post(
+      this.baseURL + url,
       {
-        headers: this.myToken
-      })
+        addressId: userAddressId,
+        paymentMethod: payInfo,
+        note: note,
+      },
+      {
+        headers: this.myToken,
+      }
+    );
   }
-
 }
