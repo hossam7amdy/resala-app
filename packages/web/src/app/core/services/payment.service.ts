d0452e2565f987bbf56751b4bee78611f275d6bc
mtaha-develop <mtaha7017@gmail.com>
2024-08-10 23:49:27 +0300

    Edit on BaseURL

--- packages/web/src/app/core/services/payment.service.ts
+++ packages/web/src/app/core/services/payment.service.ts
@@ -12,7 +12,7 @@ export class PaymentService {
   constructor(
     private _HttpClient: HttpClient
   ) { }
-  baseURL: string = `https://resala-app-6ba5cpyy5q-ey.a.run.app`;
+  baseURL: string = `http://ec2-13-49-159-109.eu-north-1.compute.amazonaws.com/`;
   myToken: any = { 'Authorization': `Bearer ${localStorage.getItem('etoken')}` }
 
   // countries API 
--- packages/web/src/app/core/services/wish-list.service.ts
+++ packages/web/src/app/core/services/wish-list.service.ts
@@ -11,7 +11,7 @@ export class WishListService {
   constructor(private _HttpClient: HttpClient) { }
 
   //base URL
-  baseUrl: string = `https://resala-app-6ba5cpyy5q-ey.a.run.app/`;
+  baseUrl: string = `http://ec2-13-49-159-109.eu-north-1.compute.amazonaws.com/`;
 
 
   // My token
