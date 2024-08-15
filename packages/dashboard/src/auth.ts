42748463c6f0f897c813530a19b89661ad5aaa54
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-08-15 18:56:33 +0300

    `refactor: Update login function to redirect to dashboard after successful login`

--- packages/dashboard/src/auth.ts
+++ packages/dashboard/src/auth.ts
@@ -1,9 +1,9 @@
-import { ENDPOINT_CONFIGS } from '@resala/shared';
+import { ENDPOINT_CONFIGS, LoginRequest, LoginResponse } from '@resala/shared';
 import NextAuth from 'next-auth';
 import credentials from 'next-auth/providers/credentials';
 
 import { authConfig } from './auth.config';
-import { Endpoint } from './services/callEndpoint';
+import { callEndpoint } from './services/callEndpoint';
 import { setCookie } from './utils/cookies';
 
 export const { auth, signIn, signOut, handlers } = NextAuth({
@@ -14,10 +14,12 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
         sign: { type: 'text' },
         password: { type: 'password' },
       },
+      // @ts-expect-error - We don't need to define the types for this function
       authorize: async credentials => {
         try {
-          const { method, url } = ENDPOINT_CONFIGS.login;
-          const { data } = await Endpoint({ method, url, data: credentials });
+          const { data } = await callEndpoint<LoginRequest, LoginResponse>(ENDPOINT_CONFIGS.login, {
+            body: credentials as LoginRequest['body'],
+          });
 
           if (!['MODERATOR', 'ADMIN'].includes(data.user.role)) {
             throw new Error('You are not authorized to access this page');
