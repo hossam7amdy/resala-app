ebdd8be8f4282fa14f868b96b267ee1e0a24bc59
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-08-23 12:38:00 +0300

    refactor: Update Paymob webhook handlers to use new data structure

--- packages/server/src/webhooks/paymob.ts
+++ packages/server/src/webhooks/paymob.ts
@@ -37,32 +37,8 @@ const updateDatabase = async (
   console.log('updateDatabase', response);
 };
 
-const verify = (hmac: string, transaction: PostProcessedBodyDTO['transaction']): boolean => {
-  const lexicographical =
-    transaction.amount_cents +
-    transaction.created_at +
-    transaction.currency +
-    transaction.error_occured +
-    transaction.has_parent_transaction +
-    transaction.id +
-    transaction.integration_id +
-    transaction.is_3d_secure +
-    transaction.is_auth +
-    transaction.is_capture +
-    transaction.is_refunded +
-    transaction.is_standalone_payment +
-    transaction.is_voided +
-    transaction.order.id +
-    transaction.owner +
-    transaction.pending +
-    transaction.source_data.pan +
-    transaction.source_data.sub_type +
-    transaction.source_data.type +
-    transaction.success;
-
-  const hash = createHmac('sha512', process.env.PAYMOB_HMAC_KEY!)
-    .update(lexicographical)
-    .digest('hex');
+const verify = (hmac: string, lexString: string): boolean => {
+  const hash = createHmac('sha512', process.env.PAYMOB_HMAC_KEY!).update(lexString).digest('hex');
 
   return hash === hmac;
 };
@@ -76,7 +52,29 @@ export const postProcess: RequestHandler<
     const orderId = +req.params.orderId;
     const { hmac, transaction } = req.body;
 
-    const isValid = verify(hmac, transaction);
+    const lexicographical =
+      transaction.amount_cents +
+      transaction.created_at +
+      transaction.currency +
+      transaction.error_occured +
+      transaction.has_parent_transaction +
+      transaction.id +
+      transaction.integration_id +
+      transaction.is_3d_secure +
+      transaction.is_auth +
+      transaction.is_capture +
+      transaction.is_refunded +
+      transaction.is_standalone_payment +
+      transaction.is_voided +
+      transaction.order.id +
+      transaction.owner +
+      transaction.pending +
+      transaction.source_data.pan +
+      transaction.source_data.sub_type +
+      transaction.source_data.type +
+      transaction.success;
+
+    const isValid = verify(hmac, lexicographical);
 
     console.log('req.body', req.body);
     console.log('isValid', isValid);
@@ -101,35 +99,34 @@ export const postRedirect: RequestHandler<
 > = async (req, res) => {
   try {
     const orderId = +req.params.orderId;
+    const transaction = req.query;
     const hmac = req.query.hmac;
 
-    const transaction: PostProcessedBodyDTO['transaction'] = {
-      amount_cents: req.query.amount_cents,
-      created_at: req.query.created_at,
-      currency: req.query.currency,
-      error_occured: req.query.error_occured,
-      has_parent_transaction: req.query.has_parent_transaction,
-      id: req.query.id,
-      integration_id: req.query.integration_id,
-      is_3d_secure: req.query.is_3d_secure,
-      is_auth: req.query.is_auth,
-      is_capture: req.query.is_capture,
-      is_refunded: req.query.is_refunded,
-      is_standalone_payment: req.query.is_standalone_payment,
-      is_voided: req.query.is_voided,
-      order: {
-        id: req.query.order,
-      },
-      owner: req.query.owner,
-      pending: req.query.pending,
-      source_data: req.query.source_data,
-      success: req.query.success,
-      receipt: '',
-    };
+    const lexicographical =
+      req.query.amount_cents +
+      req.query.created_at +
+      req.query.currency +
+      req.query.error_occured +
+      req.query.has_parent_transaction +
+      req.query.id +
+      req.query.integration_id +
+      req.query.is_3d_secure +
+      req.query.is_auth +
+      req.query.is_capture +
+      req.query.is_refunded +
+      req.query.is_standalone_payment +
+      req.query.is_voided +
+      req.query.order +
+      req.query.owner +
+      req.query.pending +
+      req.query.source_data_pan +
+      req.query.source_data_sub_type +
+      req.query.source_data_type +
+      req.query.success;
 
-    const isValid = verify(hmac, transaction);
+    const isValid = verify(hmac, lexicographical);
 
-    await updateDatabase(orderId, transaction);
+    await updateDatabase(orderId, transaction as unknown as PostProcessedBodyDTO['transaction']);
 
     if (!isValid) {
       throw new Error('Invalid HMAC');
