45de331c01f757aeec942a5073b5ce3e6aa901c3
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-09-06 12:23:05 +0300

    refactor: Add header component to layout for smaller screens and improve responsiveness and import statements for fetch module in dashboard data files

--- packages/dashboard/src/components/layout/main.tsx
+++ packages/dashboard/src/components/layout/main.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export const Main: React.FC<LayoutProps> = ({ children, ...props }) => {
   return (
     <Content
-      className="ml-[80px] lg:ml-[200px]"
+      className="ml-0 sm:ml-[80px] lg:ml-[200px]"
       style={{ marginLeft: 200, minHeight: '100vh', padding: 20 }}
       {...props}
     >
