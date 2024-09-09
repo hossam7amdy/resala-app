e9b902546b3b3ce603db2fb5bd20bff8ee543b71
mtaha-develop <mtaha7017@gmail.com>
2024-09-09 12:13:27 +0300

    forgot password developed

--- packages/web/src/app/core/services/auth.service.ts
+++ packages/web/src/app/core/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
+import { ENDPOINT_CONFIGS, Endpoints, withParams } from '../../../../../shared/src/endpoints';
 
 @Injectable({
   providedIn: 'root',
@@ -12,15 +13,15 @@ export class AuthService {
 
   signOut: boolean = false;
   userInfo: any;
-  baseURL: string = `http://ec2-13-49-159-109.eu-north-1.compute.amazonaws.com/`;
+  baseURL: string = `http://ec2-13-49-159-109.eu-north-1.compute.amazonaws.com`;
   //baseurl = https://resala-app.onrender.com/
 
   register(userData: object): Observable<any> {
-    return this._HttpClient.post(this.baseURL + 'api/v1/auth/register', userData);
+    return this._HttpClient.post(this.baseURL + '/api/v1/auth/register', userData);
   }
 
   login(userdata: any): Observable<any> {
-    return this._HttpClient.post(this.baseURL + 'api/v1/auth/login', userdata);
+    return this._HttpClient.post(this.baseURL + '/api/v1/auth/login', userdata);
   }
 
   decodeUser(): void {
@@ -31,5 +32,22 @@ export class AuthService {
       this.userInfo = decode;
       this.signOut = true;
     }
+  }
+
+
+  // Forgot password 
+  forgotPassword(forgotPwData:any):Observable<any>{
+    const {url} = withParams(ENDPOINT_CONFIGS[Endpoints.forgotPassword])
+    return this._HttpClient.post(this.baseURL+ url ,forgotPwData )
+  }
+
+  // Reset Password
+  resetPassword(resetPwData: any, token:any):Observable<any>{
+    const {url} = withParams(ENDPOINT_CONFIGS[Endpoints.resetPassword])
+    return this._HttpClient.post(this.baseURL+ url, resetPwData ,
+      {
+        headers:token
+      }
+    )
   }
 }
