d5ec8f72680ee6b32fc03ee23930c4f35cc7facd
mtaha-develop <mtaha7017@gmail.com>
2024-09-11 07:02:02 +0300

    developed post_pay page

---
+++ packages/web/src/app/components/post-pay/post-pay.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PostPayComponent } from './post-pay.component';
+
+describe('PostPayComponent', () => {
+  let component: PostPayComponent;
+  let fixture: ComponentFixture<PostPayComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PostPayComponent]
+    });
+    fixture = TestBed.createComponent(PostPayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
