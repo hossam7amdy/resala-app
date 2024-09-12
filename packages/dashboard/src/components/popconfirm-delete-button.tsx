3f9d613a6f628f00b1f9739160ab081a20bc93e1
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-09-12 09:40:42 +0300

    refactor: Rename Loading component to Fallback
    The Loading component in the dashboard has been renamed to Fallback. This change was made to address an issue with redundant RSC fetches on navigation. For more details, refer to the issue on GitHub: https://github.com/vercel/next.js/issues/65878

--- packages/dashboard/src/components/popconfirm-delete-button.tsx
+++ packages/dashboard/src/components/popconfirm-delete-button.tsx
@@ -6,8 +6,6 @@ import { Button, Popconfirm } from 'antd';
 import type { PopconfirmProps } from 'antd';
 import React from 'react';
 
-import { ResalaTooltip } from '.';
-
 interface PopconfirmDeleteButtonProps extends Omit<PopconfirmProps, 'title'> {
   title?: PopconfirmProps['title'];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -48,9 +46,7 @@ export const PopconfirmDeleteButton: React.FC<PopconfirmDeleteButtonProps> = ({
       disabled={disabled}
       {...props}
     >
-      <ResalaTooltip title="Delete">
-        <Button size="small" danger type="link" icon={<DeleteFilled />} disabled={disabled} />
-      </ResalaTooltip>
+      <Button size="small" danger type="link" icon={<DeleteFilled />} disabled={disabled} />
     </Popconfirm>
   );
 };
