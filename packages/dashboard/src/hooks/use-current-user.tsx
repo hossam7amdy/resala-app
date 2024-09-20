9543e9b6f27906419d92abb59733707fa253812a
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-09-20 17:29:45 +0300

    refactor: Update import paths for routes in multiple files

---
+++ packages/dashboard/src/hooks/use-current-user.tsx
@@ -0,0 +1,22 @@
+import { getCurrentUser } from '@/fetch/users';
+import type { User } from '@resala/shared';
+import { useEffect, useState } from 'react';
+
+export const useCurrentUser = () => {
+  const [user, setUser] = useState<User>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const user = await getCurrentUser();
+        setUser(user);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return { isLoading, user: user };
+};
