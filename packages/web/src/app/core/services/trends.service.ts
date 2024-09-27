aac618d0e1c8a018d732194302bc985852d23c54
mtaha-develop <mtaha7017@gmail.com>
2024-09-27 11:51:09 +0300

    Developed Trend Products & media query , linked What'sapp ,  ajusted all styles

---
+++ packages/web/src/app/core/services/trends.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ENDPOINT_CONFIGS, Endpoints, withParams, withQueryParams } from '../../../../../shared/src/endpoints';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrendsService {
+
+  constructor(
+    private _HttpClient: HttpClient
+  ) { }
+
+  private getHeaders() {
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning':  '69420',
+      
+    });
+    return {headers};
+  }
+
+  getTrendProducts():Observable<any>{
+    
+    return this._HttpClient.get(environment.BASE_URL+`/api/v1/dashboard/top-products`,this.getHeaders())
+  }
+}
