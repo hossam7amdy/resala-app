4766a2c4ee20638f28b45aff631bb8612bb655cc
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-10-03 00:04:19 +0300

    Refactor validation schema to include discount and common schemas

--- packages/shared/src/validations/common.schema.d.ts
+++ packages/shared/src/validations/common.schema.d.ts
@@ -1,11 +1,18 @@
 import { z } from 'zod';
-export declare const OffsetPageParamsSchema: z.ZodObject<{
+
+export declare const OffsetPageParamsSchema: z.ZodObject<
+  {
     page: z.ZodOptional<z.ZodDefault<z.ZodNumber>>;
     limit: z.ZodOptional<z.ZodDefault<z.ZodNumber>>;
-}, "strip", z.ZodTypeAny, {
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     page?: number | undefined;
     limit?: number | undefined;
-}, {
+  },
+  {
     page?: number | undefined;
     limit?: number | undefined;
-}>;
+  }
+>;
