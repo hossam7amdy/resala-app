7cc140f61e094e908642bf5ab19d43ff522d961c
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-10-06 23:26:08 +0300

    Refactor update discount related components to use separate endpoints for add/remove products

--- packages/dashboard/src/features/discounts/discount-table.tsx
+++ packages/dashboard/src/features/discounts/discount-table.tsx
@@ -12,6 +12,7 @@ import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
 import { DiscountActions } from './discount-actions';
+import { DiscountActiveStatus } from './discount-active-status';
 
 const extractFilterValueFromSearchParams = (searchParams: URLSearchParams, key: string) => {
   const value = searchParams.get(key);
@@ -91,8 +92,6 @@ export const DiscountTable: React.FC<{
           filters: [
             { text: 'BOGO', value: 'BOGO' },
             { text: 'Percentage', value: 'PERCENTAGE' },
-            { text: 'Fixed', value: 'FIXED' },
-            { text: 'Bulk', value: 'BULK' },
           ],
         },
         {
@@ -108,20 +107,6 @@ export const DiscountTable: React.FC<{
           title: 'Amount',
           dataIndex: 'amount',
           filteredValue: null,
-        },
-        {
-          width: 100,
-          align: 'center',
-          title: 'Active',
-          dataIndex: 'isActive',
-          filterMultiple: false,
-          filteredValue: extractFilterValueFromSearchParams(searchParams, 'isActive')
-            ? [searchParams.get('isActive')!]
-            : null,
-          filters: [
-            { text: 'Yes', value: true },
-            { text: 'No', value: false },
-          ],
         },
         {
           width: 100,
@@ -158,6 +143,20 @@ export const DiscountTable: React.FC<{
         },
         {
           width: 100,
+          align: 'center',
+          title: 'Active',
+          dataIndex: 'isActive',
+          filterMultiple: false,
+          filteredValue: extractFilterValueFromSearchParams(searchParams, 'isActive')
+            ? [searchParams.get('isActive')!]
+            : null,
+          filters: [
+            { text: 'Yes', value: true },
+            { text: 'No', value: false },
+          ],
+        },
+        {
+          width: 100,
           title: 'Actions',
           dataIndex: 'actions',
           filteredValue: null,
@@ -171,8 +170,10 @@ export const DiscountTable: React.FC<{
         ) : (
           <IconLink href={ROUTES.DISCOUNT_PRODUCTS(d.id)}>{d.id}</IconLink>
         ),
+        minQty: 'buy ' + d.minQty,
+        amount: 'get ' + d.amount + (d.type === 'PERCENTAGE' ? '% off' : ' free'),
         products: d.isStoreWide ? 'All' : d.productsCount,
-        isActive: d.isActive ? 'Yes' : 'No',
+        isActive: <DiscountActiveStatus discount={d} />,
         isStoreWide: d.isStoreWide ? 'Yes' : 'No',
         startDate: d.startDate ? formatDate(d.startDate) : 'N/A',
         endDate: d.endDate ? formatDate(new Date(d.endDate)) : 'N/A',
