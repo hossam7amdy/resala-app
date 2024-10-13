e2baea3c1033a32b9be3744c8175a77770b373bb
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-10-13 22:55:03 +0300

    refactor: Update docker-compose.yaml and package.json

--- packages/server/lint-staged.config.mjs
+++ packages/server/lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-  'src/**/*.{test.ts}': ['yarn test:unit'],
-  'test/**/*.{test.ts}': ['yarn test:int'],
+  'src/**/*.test.ts': ['yarn test:unit'],
+  'test/**/*.test.ts': ['yarn test:int'],
 };
