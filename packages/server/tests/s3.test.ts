d4588c965f046241410b146155600a6b86453dec
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-10-18 21:38:52 +0300

    test: Add integration test for /products endpoint and  validation for image file in uploadHandler middleware

--- packages/server/tests/s3.test.ts
+++ packages/server/tests/s3.test.ts
@@ -1,7 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 
 import { configuration } from '../src/configuration';
-import { S3Service } from '../src/services/s3/s3';
+import { S3Service } from '../src/services/s3';
 
 describe('S3Service', () => {
   let s3Service: S3Service;
