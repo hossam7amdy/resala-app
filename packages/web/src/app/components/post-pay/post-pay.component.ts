c70443096aaeca511360c7d3983d9b8d0007b9a1
mtaha-develop <mtaha7017@gmail.com>
2024-10-25 18:52:13 +0300

    complete spinner , order tracking

--- packages/web/src/app/components/post-pay/post-pay.component.ts
+++ packages/web/src/app/components/post-pay/post-pay.component.ts
@@ -1,25 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { SpinnerComponent } from 'src/app/core/spinner/spinner.component';
 
 @Component({
   selector: 'app-post-pay',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SpinnerComponent, TranslateModule],
   templateUrl: './post-pay.component.html',
   styleUrls: ['./post-pay.component.css'],
 })
 export class PostPayComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
+  // start Custome Spinner
+  customSpinIsLoading = false;
+  //end Custome Spinner
   orderStatus: string = '';
   orderId: any;
   ngOnInit(): void {
+    this.customSpinIsLoading = true;
     this.route.paramMap.subscribe(params => {
       this.orderId = params.get('orderId');
     });
     this.route.queryParams.subscribe(mobPayQuery => {
       this.orderStatus = mobPayQuery['success'];
       console.log(mobPayQuery, 'order status', this.orderStatus);
+      this.customSpinIsLoading = false;
     });
   }
 }
