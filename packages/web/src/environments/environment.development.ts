765767479a141cfd867e295e0ee598278fe5ffc8
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-10-26 21:57:15 +0300

    Update development server URL and adjust styles

--- packages/web/src/environments/environment.development.ts
+++ packages/web/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000',
-  BASE_URL: `http://ec2-13-60-17-214.eu-north-1.compute.amazonaws.com`,
+  BASE_URL: `https://www.resala.live`,
 };
