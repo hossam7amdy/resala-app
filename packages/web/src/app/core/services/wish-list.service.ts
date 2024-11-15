d34b1b3414d8399551aad3d436cdc0969a5ef9c6
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-11-15 19:39:10 +0200

    Refactor services and components for improved readability and type safety; update user service methods and remove unnecessary console logs

--- packages/web/src/app/core/services/wish-list.service.ts
+++ packages/web/src/app/core/services/wish-list.service.ts
@@ -1,6 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ENDPOINT_CONFIGS, Endpoints, withParams } from '@resala/shared';
+import {
+  CreateWishlistResponse,
+  DeleteWishlistResponse,
+  ENDPOINT_CONFIGS,
+  GetWishlistResponse,
+  withParams,
+} from '@resala/shared';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 
@@ -10,27 +16,18 @@ import { environment } from 'src/environments/environment.development';
 export class WishListService {
   constructor(private _HttpClient: HttpClient) {}
 
-  // refactor free API url
   private getHeaders() {
     const headers = new HttpHeaders({
-      'ngrok-skip-browser-warning': '69420',
       Authorization: `Bearer ${localStorage.getItem('etoken')}`,
     });
 
     return { headers };
   }
 
-  //base URL
-  // baseUrl: string = `https://api.resala.live`;
-
-  // My token
-  myToken: any = { Authorization: `Bearer ${localStorage.getItem('etoken')}` };
-
-  //post items wishtlist method
-  postWishListItems(productId: any): Observable<any> {
-    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.addProductToWishlist]);
-    return this._HttpClient.post(
-      environment.BASE_URL + url,
+  postWishListItems(productId: any): Observable<CreateWishlistResponse> {
+    const { url } = withParams(ENDPOINT_CONFIGS.addProductToWishlist);
+    return this._HttpClient.post<CreateWishlistResponse>(
+      environment.baseUrl + url,
       {
         productId: productId,
       },
@@ -38,15 +35,16 @@ export class WishListService {
     );
   }
 
-  // get all favourits products
-  getAllMyProducts(): Observable<any> {
-    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.getUserWishlist]);
-    return this._HttpClient.get(environment.BASE_URL + url, this.getHeaders());
+  getAllMyProducts(): Observable<GetWishlistResponse> {
+    const { url } = withParams(ENDPOINT_CONFIGS.getUserWishlist);
+    return this._HttpClient.get<GetWishlistResponse>(environment.baseUrl + url, this.getHeaders());
   }
 
-  //Delelte product from my favorite
-  deleteMyFavoriteProduct(id: any): Observable<any> {
-    const { url } = withParams(ENDPOINT_CONFIGS[Endpoints.removeProductFromWishlist], id + '');
-    return this._HttpClient.delete(environment.BASE_URL + url, this.getHeaders());
+  deleteMyFavoriteProduct(id: any): Observable<DeleteWishlistResponse> {
+    const { url } = withParams(ENDPOINT_CONFIGS.removeProductFromWishlist, id + '');
+    return this._HttpClient.delete<DeleteWishlistResponse>(
+      environment.baseUrl + url,
+      this.getHeaders()
+    );
   }
 }
