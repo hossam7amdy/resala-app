5d57a028bec1ce242bb027d7cc98def233ff5af6
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-11-19 08:48:02 +0200

    feat: enhance order service to include user information and implement AWS Cognito group retrieval for authorization

--- packages/server/src/middlewares/authorization.ts
+++ packages/server/src/middlewares/authorization.ts
@@ -1,7 +1,32 @@
+import {
+  AdminListGroupsForUserCommand,
+  CognitoIdentityProviderClient,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { Role, type RoleType } from '@resala/shared';
 import type { RequestHandler } from 'express';
 
+import { configuration } from '../configuration/index.js';
 import { ForbiddenError } from '../errors/api.errors.js';
+
+const getUserGroups = async (userId: string) => {
+  const client = new CognitoIdentityProviderClient({
+    region: configuration.aws.region,
+    credentials: {
+      accessKeyId: configuration.aws.accessKey,
+      secretAccessKey: configuration.aws.accessSecret,
+    },
+  });
+
+  const input = {
+    UserPoolId: configuration.aws.auth.userPoolId,
+    Username: userId,
+  };
+
+  const command = new AdminListGroupsForUserCommand(input);
+  const response = await client.send(command);
+
+  return response.Groups?.map(group => group.GroupName);
+};
 
 export const authorization: RequestHandler = (req, res, next) => {
   const { id, role } = res.locals.user ?? {};
@@ -18,11 +43,16 @@ export const authorization: RequestHandler = (req, res, next) => {
 };
 
 export const authorizeRole = (roles: RoleType[]): RequestHandler => {
-  return (_req, res, next) => {
-    const { role } = res.locals.user;
+  return async (_req, res, next) => {
+    try {
+      const groups = await getUserGroups(res.locals.user.sub);
 
-    if (!roles.includes(role)) {
-      throw new ForbiddenError();
+      const hasRole = roles.some(role => groups?.includes(role.toLowerCase()));
+      if (!hasRole) {
+        throw new Error('User does not have the required role');
+      }
+    } catch (e) {
+      return next(new ForbiddenError((e as Error).message));
     }
 
     next();
