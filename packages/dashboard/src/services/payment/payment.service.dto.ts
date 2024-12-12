109a2d31738079bdc611d393a3c9e5a203af106c
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-12-12 23:44:41 +0200

    refactor(payment): update Address import to use shared module for consistency

--- packages/dashboard/src/services/payment/payment.service.dto.ts
+++ packages/dashboard/src/services/payment/payment.service.dto.ts
@@ -1,5 +1,5 @@
-import type { Address } from '@prisma/client';
 import type { Decimal } from '@prisma/client/runtime/library';
+import type { Address } from '@resala/shared';
 
 export interface CheckoutCreateParams {
   orderId: string;
