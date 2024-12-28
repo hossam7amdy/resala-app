abc2703996fdc5ec43327095f70fb416d2e12596
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-12-28 16:47:43 +0200

    refactor: update ResalaLogo component to use external logo URL

--- packages/dashboard/src/components/ResalaLogo.tsx
+++ packages/dashboard/src/components/ResalaLogo.tsx
@@ -4,5 +4,12 @@ import type { ImageProps } from 'next/image';
 type ResalaLogoProps = Omit<ImageProps, 'src' | 'alt'>;
 
 export const ResalaLogo: React.FC<ResalaLogoProps> = ({ height = 60, width = 116, ...props }) => (
-  <Image priority src="/logo.png" alt="Resala Logo" height={height} width={width} {...props} />
+  <Image
+    priority
+    src="https://cdn.resala.live/rc-upload-1735396330648-4"
+    alt="Resala Logo"
+    height={height}
+    width={width}
+    {...props}
+  />
 );
